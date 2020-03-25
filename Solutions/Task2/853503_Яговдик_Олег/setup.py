2e13356c04e8f066746ad06a880d569876a17906
Oleg <ampeo@yandex.ru>
2020-03-25 22:44:59 +0300

    setup

---
+++ Solutions/Task2/853503_Яговдик_Олег/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Laba",
+    author="Oleg Yagovdic",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Linux",
+    ],
+    python_requires='>3.8',
+)
